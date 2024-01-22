d4f7174b4e7bc9630589cf9700c069a7aef7f519
peppercatastrophe <abyansetia@gmail.com>
2024-01-22 13:34:33 +0700

    feat: add subtask in task

--- models/Task.js
+++ models/Task.js
@@ -1,8 +1,43 @@
 class Task {
-  constructor(userId, name, sessions = []) {
+  constructor(
+    userId,
+    name,
+    sessions = [],
+    subtasks = []
+  ) {
     this.userId = userId
     this.name = name
     this.sessions = sessions
+    this.subtasks = subtasks
+  }
+
+  static create(
+    userId,
+    name,
+    sessions = [],
+    subtasks = []
+  ) {
+    return new Task(userId, name, sessions, subtasks)
+  }
+}
+
+class SubTask {
+  constructor(
+    taskId,
+    name,
+    isDone = false
+  ) {
+    this.taskId = taskId
+    this.name = name
+    this.isDone = isDone
+  }
+
+  static create(
+    taskId,
+    name,
+    isDone
+  ) {
+    return new SubTask(taskId, name, isDone)
   }
 }
 
