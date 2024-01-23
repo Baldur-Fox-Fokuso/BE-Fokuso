657ad5f77c399f198d6844002cf693db6fa9582e
peppercatastrophe <abyansetia@gmail.com>
2024-01-23 11:17:53 +0700

    feat: add create, isDone in SessionController

--- controller/session.js
+++ controller/session.js
@@ -5,23 +5,30 @@ function getDb() {
   return getCollection("tasks");
 }
 
-class SessionConstrctor {
+class SessionController {
   // static getDb() {
   //   return getCollection("session");
   // }
 
+  // TODO:
+  // input params: taskId, body: name, duration
   static async create(req, res, next) {
     try {
-      const { taskId, name, duration } = req.body
+      const { taskId, } = req.params;
+      console.log(taskId, "<-- taskId in create Session")
+      // const { name, duration } = req.body;
       // validate variables in req.body
+
+      const sessionId = new ObjectId()
 
       const filter = { _id: new ObjectId(taskId) }
       const update = {
         $push: {
           sessions: {
+            _id: sessionId,
             taskId: new ObjectId(taskId),
-            name,
-            duration,
+            // name,
+            duration: 1500,
             isDone: false,
             createdAt: new Date(),
             updatedAt: new Date()
@@ -29,22 +36,44 @@ class SessionConstrctor {
         }
       }
       const updateResult = await getDb().updateOne(filter, update)
+      console.log(updateResult, '<-- updateResult in create session')
 
       res.status(201).json({
         message: "Session created successfully",
-        data: updateResult
+        data: updateResult,
+        sessionId
       });
     } catch (error) {
-      console.log(error);
+      console.log(error, '<-- Error create session');
       next(error);
     }
   }
 
   // TODO:
-  static async getById(_id) {
+  // input params: taskId, sessionId
+  static async getById(req, res, next) {
     try {
-      const session = await getDb.findOne({ _id });
-      res.status(200).json(session);
+      // get taskId and sessionId from request params
+      const { taskId, sessionId } = res.params;
+
+      // get task with taskId
+      const task = await getDb.findOne({ _id: new ObjectId(taskId) });
+
+      // make ObjectId for _id of session
+      const sessionIdObj = new ObjectId(sessionId);
+
+      // find Session inside of Task
+      const session = task.sessions.find(el => {
+        return el._id == sessionIdObj
+      });
+
+      // if that Session exists, send in Response, else 404
+      if (session) {
+        res.status(200).json(session);
+      } else {
+        throw { code: 404 };
+      }
+
     } catch (error) {
       console.log(error);
       next(error);
@@ -52,11 +81,16 @@ class SessionConstrctor {
   }
 
   // TODO:
-  static async getByTaksId(taskId) {
+  // input params: taskId
+  static async getByTaskId(req, res, next) {
     try {
-      const sessions = await getDb
-        .find({ taskId: new ObjectId(taskId) })
-        .toArray();
+      // get taskId from request params
+      const { taskId } = res.params;
+
+      // get task with taskId
+      const task = await getDb.findOne({ _id: new ObjectId(taskId) });
+      // get the sessions
+      const sessions = task.sessions;
 
       res.status(200).json(sessions);
     } catch (error) {
@@ -65,16 +99,63 @@ class SessionConstrctor {
     }
   }
 
-  static async isDone(_id) {
+  // TODO: sessionId jadi ambil index saja
+  // input params: taskId, sessionId
+  static async isDone(req, res, next) {
     try {
-      const session = await getDb().findOne({ _id });
-      if (!session) {
-        throw { name: "not_found" };
-      }
-      await getDb().updateOne({ _id }, { $set: { isDone: true } });
-      res.status(201).json({
-        message: "Session is done",
-      });
+      // get taskId and sessionId from request params
+      const { taskId, sessionId } = req.params;
+
+      const updateResult = await getDb().updateOne({
+        "_id": new ObjectId(taskId)
+      },
+        {
+          $set: {
+            "sessions.$[s].isDone": true
+          }
+        },
+        {
+          "multi": false,
+          "upsert": false,
+          arrayFilters: [
+            {
+              "s._id": {
+                "$eq": new ObjectId(sessionId)
+              }
+            }
+          ]
+        })
+
+      res.status(200).json({
+        success: true,
+        updateResult,
+        message: "Session has been updated to Done"
+      })
+
+      // const sessionIdNum = +sessionId
+      // const update = {
+      //   sessions[sessionId]: {
+      //     isDone: true
+      //   }
+      // }
+
+      // find Session inside of Task
+      // const session = task.sessions.find(el => {
+      //   return el._id == sessionIdObj
+      // });
+
+
+      // session.isDone = true
+
+      // old codes ============
+      // const session = await getDb().findOne({ _id });
+      // if (!session) {
+      //   throw { name: "not_found" };
+      // }
+      // await getDb().updateOne({ _id }, { $set: { isDone: true } });
+      // res.status(201).json({
+      //   message: "Session is done",
+      // });
     } catch (error) {
       console.log(error);
       next(error);
@@ -82,4 +163,4 @@ class SessionConstrctor {
   }
 }
 
-module.exports = SessionConstrctor;
+module.exports = SessionController;
